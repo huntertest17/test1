eb11bc3472688cbc080c9167d0c87ecbe96c14d9
huntertest17 <jjbrin42@live.com>
2017-03-12 07:56:20 -0700

    Create setup_aws.sh

---
+++ setup_aws.sh
@@ -0,0 +1,12 @@
+# Ubuntu: install software
+sudo apt-get update
+sudo apt install python -y
+sudo apt install python-pip -y
+pip install --upgrade pip
+sudo pip install ipython
+sudo pip install jupyter
+sudo pip install scipy numpy matplotlib
+
+#### Create self-signed SSL certificate
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
+
